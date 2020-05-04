257cb60d9a6bd32d776ff9c421b539bf2a4cd1c7
403688951 <cxcuxe1355>
2020-05-04 19:13:05 +0800

    初始化项目

---
+++ springboot-gyxt/src/main/java/com/gyxt/GyxtApplication.java
@@ -0,0 +1,13 @@
+package com.gyxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyxtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyxtApplication.class, args);
+	}
+
+}
